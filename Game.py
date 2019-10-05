fda2f6e8ad7158faf6a167d6ba6a4d79b226e776
Aloin <onealainn@gmail.com>
2019-10-04 21:00:05 -0400

    Added instructions x2

--- Game.py
+++ Game.py
@@ -3,18 +3,11 @@ import guiOut as go
 import DataSet as dt
 import Algorithm as ag
 
-"""Game"""
 # Creates a board
 board = np.zeros((6,7))
 
-"""JonathanIsHere"""
-
-"""Ricc"""
-def getBoard():
-    return board
-
-"""Alain"""
 _in = int(input("In:"))
 board   = dt.update(_in, board)    # Updates the board to user's input, returns new board
+_y = dt.returnY() # Returns the y of the most recent dropped in piece
 column  = go.read(board)     # Reads the new board and compares to old board, returning column number
-ag.check(column,board)      # From the column number, check board to see if win condition met return T/F
+ag.check(_in, _y, board)      # From the column number, check board to see if win condition met return T/F
